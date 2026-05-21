832267e0868402eae069d876d99f37b0222e1eb7
Naman Chhabra <chhabranaman21@gmail.com>
2026-05-21 15:24:37 +0530

    Updated Example Code
    Updated Example code for Version 1.0.3

--- example.py
+++ example.py
@@ -1,5 +1,4 @@
 import splashscreen_engine as splash
-import threading
 
 screen = splash.Screen()
 screen.size(750,500)
@@ -8,7 +7,13 @@ screen.size(750,500)
 screen.start()
 
 # Background Video
-video = splash.BackgroundVideo(screen,"exampleVid.mp4",fps=30,loop=True)
+video = splash.BackgroundVideo(
+    screen,
+    "exampleVid.mp4",
+    fps=30,
+    loop=True
+)
+
 video.play()
 
 # Loading Bar
@@ -16,11 +21,15 @@ bar = splash.LoadingBar(screen)
 bar.place()
 
 # Text `loading`
-text = splash.Text(screen,"Loading...","impact",20,(325,400))
-text.show()
+text = splash.Text(
+    screen,
+    "Loading...",
+    "impact",
+    20,
+    (325,400)
+)
 
-# The screen will not stop and you can perform other processes
-threading.Thread(target=screen.mainloop).start()
+text.show()
 
 # LOADING | you can add your `loading` processes here
 
@@ -39,25 +48,34 @@ while not a >= 100:
     screen.wait(0.05)
 
 text.edit(
-        text=f"loaded : 100%"
-    )
+    text="loaded : 100%"
+)
 
 screen.wait(3)
 
-screen.stop() # Stop the splash screen after loading
+# Stop the splash screen after loading
+screen.stop()
 
 
 """
-if you are using pygame module in your own code, use ` screen.stop(quit_pygame=False) ` instead of ` screen.stop() `
+if you are using pygame module in your own code,
+use `screen.stop(quit_pygame=False)`
+instead of `screen.stop()`
 """
+
+# Main Screen Example
 
 import tkinter # pip install tkinter -- used as main screen for example.
 main_screen = tkinter.Tk()
+
 main_screen.geometry("750x500")
-main_text = tkinter.Label(main_screen,text="Your Main Screen",font=("impact",40))
+
+main_text = tkinter.Label(
+    main_screen,
+    text="Your Main Screen",
+    font=("impact",40)
+)
+
 main_text.pack()
 
-main_text.mainloop()
-
-
-
+main_screen.mainloop()
