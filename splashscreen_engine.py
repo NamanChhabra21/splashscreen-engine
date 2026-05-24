2498f47d4b323b8495788c7241b17da02f443fe3
Naman Chhabra <chhabranaman21@gmail.com>
2026-05-24 12:24:55 +0530

    Update v2.0.0
    - Added support for dynamic window resizing and title bar options.
    - Added text and loading bar positions
    - Bug Fixes and error handling

--- splashscreen_engine.py
+++ splashscreen_engine.py
@@ -2,6 +2,7 @@ import os
 import threading
 import time
 
+
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 s = '1'
 os.environ['SDL_VIDEO_CENTERED'] = s
@@ -9,31 +10,52 @@ os.environ['SDL_VIDEO_CENTERED'] = s
 # Modules
 import pygame
 import cv2
-
 pygame.init()
 
+deleted_by_user = False
+program_stopped = False
 
 def avoid_lag():
-
+    global deleted_by_user
+    if deleted_by_user:
+        raise RuntimeError("Failed to update UI, the screen is deleted by either you or the user.")
     pygame.event.pump()
+
 
 
 def draw_loading_bar(screen, bars):
 
     for bar in bars:
-
         if not isinstance(bar, LoadingBar):
             continue
 
         if bar.visible:
+            screen_size = screen.get_size()
+            if bar.position == "center":
+                x = (screen_size[0]-bar.width)//2 + bar.add_xy[0] # Center + x , where x can be positive or negative
+                y = (screen_size[1]-bar.height) // 2 + bar.add_xy[1]  # Center + y , where y can be positive or negative
+
+            elif bar.position == "right":
+                x = screen_size[0] - bar.width + bar.add_xy[0]
+                y = (screen_size[1]-bar.height) // 2 + bar.add_xy[1]
+            elif bar.position == "left":
+                x =bar.add_xy[0]
+                y = screen_size[1] // 2 + bar.add_xy[1]
+            elif bar.position == "up":
+                x = (screen_size[0]-bar.width)//2 + bar.add_xy[0]
+                y = bar.height + bar.add_xy[1]
+            elif bar.position == "down":
+                x = (screen_size[0]-bar.width)//2 + bar.add_xy[0]
+                y = screen_size[1]- bar.height + bar.add_xy[1]
+            else:
+                x,y=0,0
 
             # OUTER BAR
             pygame.draw.rect(
                 screen,
                 bar.colour,
                 (
-                    int(bar.x),
-                    int(bar.y),
+                    x,y,
                     int(bar.width),
                     int(bar.height)
                 ),
@@ -44,9 +66,7 @@ def draw_loading_bar(screen, bars):
             pygame.draw.rect(
                 screen,
                 bar.loading_colour,
-                (
-                    int(bar.x),
-                    int(bar.y),
+                (x,y,
                     int(
                         bar.width *
                         (bar.progress / 100)
@@ -58,32 +78,40 @@ def draw_loading_bar(screen, bars):
 
 
 def draw_text(screen, texts):
-
     for txt in texts:
-
         if not isinstance(txt, Text):
             continue
 
         if txt.visible:
+            surface = txt.pg_font.render(txt.text, True, txt.colour)
 
-            surface = txt.pg_font.render(
-                txt.text,
-                True,
-                txt.colour
-            )
-
-            screen.blit(
-                surface,
-                txt.position
-            )
+            screen_size = screen.get_size()
+            if txt.position == "center":
+                x = screen_size[0]//2 + txt.add_xy[0] # Center + x , where x can be positive or negative
+                y = screen_size[1] // 2 + txt.add_xy[1]  # Center + y , where y can be positive or negative
+            elif txt.position == "right":
+                x = screen_size[0] - surface.get_size()[0] + txt.add_xy[0]
+                y = screen_size[1] // 2 + txt.add_xy[1]
+            elif txt.position == "left":
+                x = surface.get_size()[0] + txt.add_xy[0]
+                y = screen_size[1] // 2 + txt.add_xy[1]
+            elif txt.position == "up":
+                x = screen_size[0]//2 + txt.add_xy[0]
+                y = surface.get_size()[1] + txt.add_xy[1]
+            elif txt.position == "down":
+                x = screen_size[0]//2 + txt.add_xy[0]
+                y = screen_size[1]-surface.get_size()[1] + txt.add_xy[1]
+            else:
+                x,y=0,0
+            text_rect = surface.get_rect(center=(x,y))
+            screen.blit(surface,text_rect)
 
 
 one_time_warning = True # A variable used for printing warning inside the size() function | Ensuring doesn't repeat printing the same
 
 class Screen:
 
-    def __init__(self):
-
+    def __init__(self,title_bar=False):
         # Default Height and Width
         self.height = 500
         self.width = 750
@@ -121,123 +149,121 @@ class Screen:
         # GLOBAL UI
         self.ui_elements = []
 
+        self.title_bar = title_bar
+        self.is_escape = False
+
+
+
+    def get_size(self):
+        return self.width,self.height
+
     def start(self):
 
         self.running = True
 
-        # FULLSCREEN
+        # CREATING SCREEN
         if self.fullscreen:
-
-            self.screen = pygame.display.set_mode(
-                (
-                    self.info.current_w,
-                    self.info.current_h
-                ),
-                pygame.NOFRAME
-            )
-
-        # NORMAL WINDOW
+            # For Full Screen
+            w, h = 0, 0
+            self.is_escape = False # Reset Escape
         else:
+            # For Normal Window
+            w, h = self.width, self.height
 
-            self.screen = pygame.display.set_mode(
-                (
-                    self.width,
-                    self.height
-                ),
-                pygame.NOFRAME
-            )
+        if not self.title_bar:
+            # Creates window without title Bar
+            self.screen = pygame.display.set_mode((w, h), pygame.NOFRAME)
+        else:
+            # Creates window with title Bar
+            self.screen = pygame.display.set_mode((w, h),pygame.RESIZABLE)
+
 
         self.screen.fill(self.bgColor)
 
         pygame.display.update()
 
+        # BACKGROUND MAINLOOP
         def mainloop():
-
+            global program_stopped
 
             if not self.screen:
-
                 self.start()
 
             self.running = True
 
             while self.running and not self.stopped:
+
+                # Updates window size every time you resize
+                if self.title_bar:
+                    size = pygame.display.get_window_size()
+                    self.width,self.height = size[0],size[1]
                 avoid_lag()
+
+                if self.title_bar and self.fullscreen and not self.is_escape:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+
+                            program_stopped = True
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_ESCAPE:
+                                self.is_escape = True
+                                break
+
+                else:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            program_stopped = True
+
+
 
                 # CLEAR SCREEN
                 self.screen.fill(self.bgColor)
 
                 # DRAW BACKGROUND IMAGE
                 if self.current_background:
-
-                    self.screen.blit(
-                        self.current_background.image,
-                        (0, 0)
-                    )
-
-                    draw_loading_bar(
-                        self.screen,
-                        self.current_background.ui_elements
-                    )
-
-                    draw_text(
-                        self.screen,
-                        self.current_background.ui_elements
-                    )
+                    self.screen.blit(self.current_background.image,(0, 0))
+                    draw_loading_bar(self.screen,self.current_background.ui_elements)
+                    draw_text(self.screen,self.current_background.ui_elements)
 
                 # DRAW FOREGROUND VIDEO
                 if self.foreground_video:
 
                     # DRAW VIDEO FRAME
                     if self.foreground_video.frame:
-
-                        self.screen.blit(
-                            self.foreground_video.frame,
-                            (0, 0)
-                        )
+                        self.screen.blit(self.foreground_video.frame,(0, 0))
 
                     # DRAW VIDEO UI
-                    draw_loading_bar(
-                        self.screen,
-                        self.foreground_video.ui_elements
-                    )
-
-                    draw_text(
-                        self.screen,
-                        self.foreground_video.ui_elements
-                    )
+                    draw_loading_bar(self.screen,self.foreground_video.ui_elements)
+                    draw_text(self.screen,self.foreground_video.ui_elements)
 
                 # DRAW GLOBAL UI
-                draw_loading_bar(
-                    self.screen,
-                    self.ui_elements
-                )
-
-                draw_text(
-                    self.screen,
-                    self.ui_elements
-                )
-
+                draw_loading_bar(self.screen,self.ui_elements)
+                draw_text(self.screen,self.ui_elements)
                 pygame.display.update()
 
                 self.clock.tick(60)
+            global deleted_by_user
+            deleted_by_user = True
         threading.Thread(target=mainloop).start() # Starts Mainloop as background process
 
     def stop(self,quit_pygame = True):
 
+        # Stops the Window
         self.running = False
-
         self.stopped = True
 
-        # STOP VIDEO TOO
+        # STOP VIDEO if playing
         if self.foreground_video:
-
             self.foreground_video.stop = True
+
+        # Checks if user want to quit pygame or not
         if quit_pygame:
             pygame.quit()
 
 
 
     def size(self, width=750, height=500, fullscreen=False):
+
 
         # Check if width and height is integer and more than 0
         if (
@@ -250,7 +276,7 @@ class Screen:
                 width < 1
         ):
             raise TypeError(
-                "Width and Height must be positive integers."
+                f"Width and Height must be positive integers. Got width = {width} and height = {height}."
             )
 
         global one_time_warning
@@ -266,15 +292,24 @@ class Screen:
                 "window resizing is intended."
                 "\033[0m"
             )
-            one_time_warning = False
+        one_time_warning = False
 
         self.fullscreen = fullscreen
 
-        if not fullscreen:
+        if self.fullscreen:
+            self.width,self.height = 0,0
+        else:
+            self.width,self.height = width,height
+        if self.title_bar:
+            screen_mode = pygame.RESIZABLE
+        else:
+            screen_mode = pygame.NOFRAME
 
-            self.height = height
-            self.width = width
-        self.screen = pygame.display.set_mode((width,height),pygame.NOFRAME)
+        self.screen = pygame.display.set_mode((self.width,self.height),screen_mode)
+
+
+
+
 
     def title(self, text="Splash Screen"):
 
@@ -304,6 +339,19 @@ class Screen:
     def set_bg_color(self, color=(0, 0, 0)):
 
         self.bgColor = color
+
+
+    def is_quit(self):
+        if not self.title_bar:
+            raise RuntimeError("`is_quit` only works if you enable Title Bar.")
+
+        return program_stopped # True if quit else False
+    def is_escaped(self):
+        if not self.title_bar:
+            raise RuntimeError("`is_escaped` only works if Title Bar is enabled.")
+        escape = self.is_escape
+        self.is_escape = False
+        return escape # True if escape else False
 
 
 class BackgroundVideo:
@@ -534,7 +582,11 @@ class BackgroundImage:
 
 class LoadingBar:
 
-    def __init__(self, parent, width=None, height=None):
+    def __init__(self, parent, width=None, height=None,position="center",add_xy = (0,0)):
+
+        check_valid_pos(position,"LoadingBar")
+        self.position = position.lower()
+        self.add_xy = add_xy  # adds / subtract the value of x-axis and y-axis from the chosen position
 
         self.parent = parent
 
@@ -568,10 +620,6 @@ class LoadingBar:
 
         self.width = width
         self.height = height
-
-        self.y = None
-        self.x = None
-
         self.colour = (255, 255, 255)
 
         self.loading_colour = (0, 255, 0)
@@ -582,29 +630,9 @@ class LoadingBar:
 
     def place(
             self,
-            x=None,
-            y=None,
             colour=(255, 255, 255),
             loading_colour=(0, 255, 0)
     ):
-
-        # CENTER X
-        if x is None:
-
-            x = (
-                self.screen_dimension[0]
-                - self.width
-            ) / 2
-
-        # LOWER CENTER
-        if y is None:
-
-            y = (
-                self.screen_dimension[1] * 0.75
-            ) - (self.height / 2)
-
-        self.x = int(x)
-        self.y = int(y)
 
         self.colour = colour
 
@@ -627,63 +655,46 @@ class LoadingBar:
 
         self.progress = value
 
+def check_valid_pos(string,object_type):
+    available_position = ["right", "left", "down", "up", "center",None]
+    if string not in available_position:
+        available_position.pop() # Remove `None` for displaying Available positions
+        raise RuntimeError(f"`{object_type}` object got unknown positional argument. Please choose from {available_position}")
 
 class Text:
 
-    def __init__(
-            self,
-            parent,
-            text="Your Text Here",
-            font=None,
-            size=20,
-            position=(0, 0),
-            colour=(255, 255, 255)
-    ):
+    def __init__(self,parent,text="Your Text Here",font=None,size=20,position="center",add_xy = (0,0),colour=(255, 255, 255)):
+
+        check_valid_pos(position,"Text")
+        self.position = position.lower()
+        self.add_xy = add_xy  # adds / subtract the value of x-axis and y-axis from the chosen position
+
 
         self.parent = parent
-
-
-
         self.text = text
-
         self.font = font
-
         self.size = size
-
         self.position = position
-
         self.colour = colour
-
         # DEFAULT FONT
-        self.pg_font = pygame.font.SysFont(
-            self.font,
-            self.size
-        )
-
+        self.pg_font = pygame.font.SysFont(self.font,self.size)
         self.visible = True
+
+
 
         # REGISTER
         if hasattr(self.parent, "ui_elements"):
-            self.parent.ui_elements.append(
-                self
-            )
+            self.parent.ui_elements.append(self)
 
-
-
-    def edit(
-            self,
-            text=None,
-            font=None,
-            new_size=None,
-            position=None,
-            colour=None
-    ):
+    def edit(self,text=None,font=None,new_size=None,position=None,add_xy=None,colour=None):
 
         if text is not None:
             self.text = text
 
         if font is not None:
             self.font = font
+            # UPDATE FONT
+            self.pg_font = pygame.font.SysFont(self.font, self.size)
 
         if new_size is not None:
             self.size = new_size
@@ -691,19 +702,15 @@ class Text:
         if position is not None:
             self.position = position
 
+        if add_xy is not None:
+            self.add_xy = add_xy
+
         if colour is not None:
             self.colour = colour
 
-        # UPDATE FONT
-        self.pg_font = pygame.font.SysFont(
-            self.font,
-            self.size
-        )
-
     def hide(self):
-
         self.visible = False
 
     def show(self):
-
         self.visible = True
+
